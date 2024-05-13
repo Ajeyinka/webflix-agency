ce1441c740ef488e9c118ed9af0af8f333502f2a
ajeyinka <ajeyinka12@gmail.com>
2024-05-13 11:35:32 +0100

    ghh

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
         const navLinks = document.getElementsByClassName('navbar-links')[0]
         const barOne = document.getElementsByClassName('bar one')[0]
@@ -71,3 +86,6 @@ const toggleButton = document.getElementsByClassName('toggle-button')[0]
           
         }
       })
+
+
+
